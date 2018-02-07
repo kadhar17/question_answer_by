cf3b33fd614960c38ee34269f86feea3235ad52a
abdul <kadhar17@gmail.com>
2018-02-07 15:37:59 +0530

    finaldemo for questionandsolution

--- js/app.js
+++ js/app.js
@@ -1,39 +1,51 @@
 var app=angular.module("myapp",[]);
 
-app.run(function($rootScope){
+app.run(function($rootScope,$location, $window){
+
   $rootScope.title="Step by Step";
   $rootScope.appId='';
+  $rootScope.href=$window.location.href;
+  $rootScope.params=$rootScope.href.split('?');
+  $rootScope.id=$rootScope.params[1];
+  //alert( $rootScope.id);
 })
 
 app.controller("mycontroller",function($scope,$http,$rootScope, $interval,$timeout){
 
+  $http.get("./"+$rootScope.id+".json").then(function(response){
 
- $http.get("./data.json").then(function(response){
-
-  var myObj = {}
-  var key = 0;
-  for(var i in response.data)
-  {
-    for(var j in response.data[i])
+    var myObj = {}
+    var key = 0;
+    for(var i in response.data)
     {
-
-      if(key !=0)
+      for(var j in response.data[i])
       {
-        myObj[key] = response.data[i][j];
+
+        if(key !=0)
+        {
+          myObj[key] = response.data[i][j];
+        }
+        key++;
       }
-      key++;
     }
+
+    console.log(myObj)
+    console.log(key)
+    $scope.mydata=myObj;  
+    $rootScope.appId=response.data.appId[0];
+    totobjFn(key-1)
+    hideFn()
+
+  })
+
+
+
+});
+
+app.filter('renderHTMLCorrectly', function($sce)
+{
+  return function(stringToParse)
+  {
+    return $sce.trustAsHtml(stringToParse);
   }
-
-  console.log(myObj)
-  console.log(key)
-  $scope.mydata=myObj;  
-  $rootScope.appId=response.data.appId[0];
-  totobjFn(key-1)
-  hideFn()
-
-})
-
-
-
 });
--- js/main.js
+++ js/main.js
@@ -8,9 +8,74 @@ var waitVar=true;
 var playcnt=0;
 var totobj;
 var initial=false;
-var fadeIntimer=500;
+var appeartime=1000;
+var fadeIntimer=300;
+var eachdatashow=500;
+var currentpartfade=100;
+var currentparthide=200;
+var nextpartshow=300;
+var finalfadeout=300;
+var finalfadein=600;
 
 $(document).ready(function(){
+
+	$(window).resize(function(){
+		resizeFn()
+	})
+
+
+	$("#play").bind("click",function(e){
+		playFn()
+	});
+
+	$("#modelbtn").bind("click",function(){
+		game=false;
+	})
+
+	$(".close").bind("click",function(){
+		game=true;
+	})
+
+	
+
+	$("#prev").bind("click",function(){
+		if(currentDiv>1){
+			currentDiv-=1;
+		}
+		//console.log("prev "+currentDiv)
+	});
+
+	$("#next").bind("click",function(){
+		if(currentDiv<(totalDivs)){
+			currentDiv+=1;
+		}
+		//console.log("next "+currentDiv)
+	});
+
+
+	$("#replay").bind("click",function(e){
+		console.log("replay")
+		clearInterval(chainInterval);
+		game=false;
+		playcnt=0;
+		hideFn()
+		initial=false;
+		$("#showsolution").css("display","none");
+		//$("#play").trigger("click");
+		playFn()
+		$("#play").css("display","none")
+		$("#play").text("Pause");
+		$("#replay").css("display","none");
+		$("#play").css("visibility","visible")
+		$("#prev").css("visibility","hidden");
+		$("#next").css("visibility","hidden");
+	});
+
+
+	$("#showsolution").bind("click",function(e){
+		console.log("showsolution")
+		showsolutionFn()
+	});
 
 	$("#prev").css("display","none");
 	$("#next").css("display","none");
@@ -18,104 +83,61 @@ $(document).ready(function(){
 	$("#next").css("visibility","hidden");
 	$("#replay").css("display","none");
 	$("#showsolution").css("display","none");
-
-	$(window).resize(function(){
-		console.log($(".container").css("width")+" "+window.innerHeight);
-		console.log("main "+ $("#main div").length)
-		var cpos=$(".container").css("width");
-		$("#main").css("height",(window.innerHeight-(window.innerHeight/5))+"px")
-
-	})
-
-	$(window).trigger("resize");
-
-	$("#play").click(function(e){
-		if(!game){
-			console.log("game true")
-			game=true;
-			if(!initial){
-				initial=true;
-				$("#showsolution").trigger("click");
-				$("#showsolution").css("display","block");
-				$("#main").css("overflow-y","auto");
-			}
-			$(this).text("Pause");
-			$("#prev").css("visibility","hidden");
-			$("#next").css("visibility","hidden");
-		}else{
-			console.log("game false")
-			$("#main").css("overflow-y","hidden");
-			$(this).text("Play");
-			$("#prev").css("visibility","visible");
-			$("#next").css("visibility","visible");
-			game=false;
-		}
-
-	});
-
-	$("#modelbtn").click(function(){
-		game=false;
-	})
-
-	$(".close").click(function(){
-		game=true;
-	})
-
-	
-
-	$("#prev").click(function(){
-		if(currentDiv>1){
-			currentDiv-=1;
-		}
-		console.log("prev "+currentDiv)
-	});
-
-	$("#next").click(function(){
-		if(currentDiv<(totalDivs)){
-			currentDiv+=1;
-		}
-		console.log("next "+currentDiv)
-	});
-
-
-	$("#replay").click(function(e){
-		clearInterval(chainInterval);
-		game=false;
-		playcnt=0;
-		hideFn()
-		initial=false;
-		$("#showsolution").css("display","none");
-		$("#play").trigger("click");
+	setTimeout(function(){
+		resizeFn()
+		playFn()
 		$("#play").css("display","none")
+
+	},500)
+	
+});
+
+
+function resizeFn(){
+	//console.log($(".container").css("width")+" "+window.innerHeight);
+	//console.log("main "+ $("#main div").length)
+	var cpos=$(".container").css("width");
+	$("#main").css("height",(window.innerHeight-(window.innerHeight/5))+"px")
+}
+
+function showsolutionFn(){
+	$("#mydata"+playcnt).fadeOut(fadeIntimer,"",function(){
+		$("#mydata"+playcnt).css("display","none")
+	});
+	setTimeout(function()
+	{
+		resetFn();
+	},fadeIntimer)
+
+	$("#showsolution").css("display","none");
+	$("#play").css("display","block")
+	$("#main").css("overflow-y","hidden");
+}
+
+function playFn(){
+	if(!game){
+		//console.log("game true")
+		game=true;
+		if(!initial){
+			initial=true;
+			//$("#showsolution").trigger("click");
+			showsolutionFn()
+			$("#showsolution").css("display","block");
+			$("#main").css("overflow-y","auto");
+		}
 		$("#play").text("Pause");
-		$("#replay").css("display","none");
-		$("#play").css("visibility","visible")
 		$("#prev").css("visibility","hidden");
 		$("#next").css("visibility","hidden");
-		//$("#banner").css("display","block")
-		//chainInterval = setInterval(showChain, setSpeed);
-	});
-
-
-	$("#showsolution").click(function(e){
-		$("#mydata"+playcnt).fadeOut(fadeIntimer,"",function(){
-			$("#mydata"+playcnt).css("display","none")
-		});
-		setTimeout(function()
-		{
-			resetFn();
-		},fadeIntimer)
-
-		$("#showsolution").css("display","none");
-		$("#play").css("display","block")
+	}else{
+		//console.log("game false")
 		$("#main").css("overflow-y","hidden");
-		//$("#play").trigger("click");
-	})
+		$("#play").text("Play");
+		$("#prev").css("visibility","visible");
+		$("#next").css("visibility","visible");
+		game=false;
+	}
 
-	$("#play").trigger("click");
-	$("#play").css("display","none")
-
-});
+}
 
 function hideFn(){
 	for(var i=1;i<=totobj;i++){
@@ -136,21 +158,22 @@ function totobjFn(val){
 
 function showobjFn(){
 	$("#mydata"+playcnt).css("display","none")
-	$("#mydata"+playcnt).fadeIn(fadeIntimer,"",function(){
-		console.log("callback");
-		setTimeout(animFn,fadeIntimer)
-	});
+	setTimeout(function(){
+		$("#mydata"+playcnt).css("display","none")
+		animFn()
+	},fadeIntimer)
 }
 
 function animFn(){
-	$("#mydata"+playcnt).fadeIn(fadeIntimer,"",function(){
+	$("#mydata"+playcnt).fadeIn();
+	setTimeout(function(){
 		$("#mydata"+playcnt).css("display","block")
-	});
+	},appeartime)
 
 	if(playcnt>1){
 		totalDivs = $("#solution"+(playcnt)+"> div").length;
 		$("#solution"+(playcnt)+" > div").css("display","none")
-		console.log("totalDivs "+totalDivs)
+		//console.log("totalDivs "+totalDivs)
 		game=true;
 		chainInterval = setInterval(showChain, setSpeed,playcnt);  
 	}
@@ -164,7 +187,7 @@ function resetFn(){
 	game=false;
 	playcnt+=1;
 	showobjFn()
-	console.log("playcnt "+playcnt)
+	//console.log("playcnt "+playcnt)
 }
 
 function waitingFn(){
@@ -172,37 +195,46 @@ function waitingFn(){
 }
 
 function showChain(idcnt) { 
-	console.log("timeinterval "+setSpeed+" "+game+" "+idcnt)
+	//console.log("timeinterval "+setSpeed+" "+game+" "+idcnt)
 	if(game && waitVar){
 		if (currentDiv < totalDivs) {
 
 			waitVar=false
 
-			$("#solution"+ idcnt+"> div:eq(" + currentDiv + ")").fadeIn(1000);
+			$("#solution"+ idcnt+"> div:eq(" + currentDiv + ")").fadeIn(eachdatashow);
 
 			increaseval+=$("#solution"+ idcnt+"> div:eq(" + currentDiv + ")").height();
 			//$("#main").animate({ scrollTop: (increaseval+"px") },1000);
 			var eachdiv=$("#solution"+ idcnt+"> div:eq(" + currentDiv + ")").height()
 			currentDiv++;
-			console.log("data "+$("#solution"+ idcnt+"> div:eq(" + currentDiv + ")").text().length);
+			//console.log("data "+$("#solution"+ idcnt+"> div:eq(" + currentDiv + ")").text().length);
 			var eachdata=$("#solution"+ idcnt+"> div:eq(" + (currentDiv-1) + ")").text().length;
+
+			if(eachdata>20){
+				setSpeed=3000;
+			}else{
+				setSpeed=1000;
+			}
 			
-			setSpeed=1000;
 
 			setTimeout(waitingFn,setSpeed)
 		} else {
-			console.log("clearInterval"+totobj)
+			//console.log("clearInterval"+totobj)
 			game=false;
 			clearInterval(chainInterval);
 
 			if(playcnt<totobj){
-				$("#mydata"+playcnt).fadeOut(fadeIntimer,"",function(){
+				$("#mydata"+playcnt).fadeOut(currentpartfade);
+
+				setTimeout(function()
+				{
 					$("#mydata"+playcnt).css("display","none");
-				});
+				},currentparthide);
+
 				setTimeout(function()
 				{
 					resetFn()
-				},fadeIntimer);
+				},nextpartshow);
 
 			}else{
 				
@@ -211,16 +243,12 @@ function showChain(idcnt) {
 				$("#prev").css("visibility","hidden");
 				$("#next").css("visibility","hidden");
 				
-				$("#main").fadeOut(fadeIntimer,"",function(){
+				$("#main").fadeOut(finalfadeout,"",function(){
 					showFn()
-					$("#main").css("overflow-y","scroll");
+					$("#main").css("overflow-y","auto");
 				});
-
-				$("#main").fadeIn(1000,"",function(){
-					
-				});
-				//$("html, body").animate({ scrollTop: 0 }, 5000);
-				//$("#main").animate({ scrollTop: (0) },5000);
+				$("#main").fadeIn(finalfadein);
+				
 			}
 		}
 	}
